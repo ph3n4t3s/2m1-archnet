bd290bb627bf51486f379a43afc2c162860f893d
ph3n4t3s <105867089+ph3n4t3s@users.noreply.github.com>
2024-04-21 22:21:59 +0200

    Mise à jour main.blocks, main.ts, 2M1-ArchNet.md

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+    serial.writeValue("touche", 1)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+    serial.writeValue("touche", 2)
+})
 basic.forever(function () {
-	
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
 })
